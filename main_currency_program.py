f9fb312d10a03f6b4f61adae35fdf7b51a5897d5
Julio Cardoza <julio_jr3@hotmail.com>
2016-04-05 14:14:57 -0400

    New version

--- main_currency_program.py
+++ main_currency_program.py
@@ -1,24 +1,53 @@
-
-
 from currency import Currency
-​
-def main():
-    one_dollar = Currency(1, 'USD')
-    two_dollar = Currency(2, 'USD')
-    three_dollar = one_dollar + two_dollar
-    print('addition:', three_dollar.amount, three_dollar.currency_code)
-​
-    # instances of the class Currency
 
-    us_dollar = Currency(1.0, "USD")
-    japanese_yen = Currency(111.32, 'YEN')
-    fijian_dollar = Currency(2.08, 'FD')
-    british_pound = Currency(0.70, "Pound")
-​
-    # converter = CurrencyConverter({'USD': 1.0, 'EUR': 0.84})
-    # converted_currency = converter.convert(one_dollar, japanese_yen)
-    # print(converted_currency)
-​
-​
+from currency_converter import CurrencyConverter
+
+def main():
+    currency_dict = {'USD':1.0, 'EUR':1.14, 'JPY':0.009}
+    #takes input in amount (needs to differentiate type from string IE $, € [option + shift + 2])
+    while True:
+        user_amount = input("Please enter an amount to convert: ")
+        if '$' in user_amount:
+            code1 = 'USD'
+            user_amount = user_amount.replace('$', '')
+            break
+        elif '€' in user_amount:
+            code1 = 'EUR'
+            user_amount = user_amount.replace('€', '')
+            break
+        elif '¥' in user_amount:
+            code1 = 'JPY'
+            user_amount = user_amount.replace('¥', '')
+            break
+        else:
+            print("Please enter dollars, euros, or yen only!")
+            continue
+
+    user_amount = float(user_amount)
+    currency_class_variable = Currency(code1, user_amount)
+    print(str(currency_class_variable))
+    while True:
+        code2 = input("Please enter a currency to convert to ($, €, ¥): ")
+        #takes second input for conversion to another currency
+        if code2 == '$':
+            code2 = 'USD'
+            break
+        elif code2 == '€':
+            code2 = 'EUR'
+            break
+        elif code2 == '¥':
+            code2 = 'JPY'
+            break
+        else:
+            print("Please enter dollars, euros, or yen only!")
+            continue
+
+    print(code2)
+    currency_converter = CurrencyConverter(currency_dict)
+    converted_currency = currency_converter.convert2(currency_class_variable, code2)
+    converted_amount = converted_currency.amount
+    print("The converted amount is: {} {}".format((float("{0:.2f}".format(converted_amount))), code2))
+    print('Current exchange rate: {}'.format(currency_converter.get_rate(currency_class_variable.code, code2)))
+
 if __name__ == '__main__':
     main()
